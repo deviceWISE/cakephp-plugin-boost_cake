97ea2277c4cab86ab385c94c6b71ddfb2bfc5ed1
Nicholas Persad <nicholas.persad@telit.com>
2017-09-29 15:37:59 -0400

    Fixes to select2.

--- View/Helper/BoostCakeFormHelper.php
+++ View/Helper/BoostCakeFormHelper.php
@@ -145,7 +145,7 @@ class BoostCakeFormHelper extends FormHelper
         }
 
         if (stristr($html, '<select')) {
-          $html = preg_replace('/<select name="(.*?)" class="(.*?)"/', '<select name="${1}" class="${2}"', $html);
+          $html = preg_replace('/<select name="(.*?)" class="(.*?)"/', '<select name="${1}" class="select-block ${2}"', $html);
           $html = str_replace('</label>', '</label><br />', $html);
         }
 
